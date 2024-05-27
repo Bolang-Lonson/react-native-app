ed0effed80d5f79fa85edeee4e5ae3a85f5b8b09
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-05-27 12:42:49 +0100

    dynamic route with square braces in filename similar to next.js

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import { Tabs, Redirect } from 'expo-router';
 
 const TabLayout = () => {
   return (
