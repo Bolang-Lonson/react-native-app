a375df2b7cdc7d5df95b5d4cd7a96621efa69218
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-05-27 12:27:39 +0100

    initial main layout and routing implemented with various views in files for file-based routing

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Profile</Text>
+      <Text>my nigga Saadiq</Text>
+    </View>
+  )
+}
+
+export default Profile
