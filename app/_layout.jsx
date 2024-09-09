ec1ea2ff4f1042eb4a5889c3f9fa85ccec86da7c
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-09-09 08:31:02 +0100

    implemented global state provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
 
+import GlobalProvider from '../context/GlobalProvider'
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -25,13 +26,16 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
     
   return (
-    <Stack>
+    <GlobalProvider>
+      <Stack>
         <Stack.Screen name="index" options={{ headerShown: false}}/>
         <Stack.Screen name="(auth)" options={{ headerShown: false}}/>
         <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
         {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false}}/> */}
-    </Stack>
+      </Stack>
+    </GlobalProvider>
+    
   )
 }
 
-export default RootLayout
+export default RootLayout;
